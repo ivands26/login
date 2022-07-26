119154ee1709049b04abe0ce7464754168fac057
ivands26 <ivandwis.fis12@gmail.com>
2022-07-26 16:14:02 +0700

    huhu

--- feature/user/repository/query.go
+++ feature/user/repository/query.go
@@ -18,9 +18,12 @@ func (ur *userRepository) Insert(newUser domain.User) (domain.User, error) {
 
 }
 func (ur *userRepository) GetForLogin(email string, password string) (domain.User, error) {
-	//line 1
-	//line 2
+	//line 10
+	//--------- 22
 	//line 99
+	// nambah lagi
+	// terus nambah lagi
+	// enak nambah terus
 }
 func (ur *userRepository) GetByID(idUser int) (domain.User, error) {
 
